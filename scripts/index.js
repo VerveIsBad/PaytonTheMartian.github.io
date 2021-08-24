435dfc464c653bb16062e0713a707820fe786906
Payton <coatspayton14@gmail.com>
2021-08-24 14:42:09 +0000

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function toggleAboutMe() {
+    if (document.getElementById('about-me-text').style.display == 'none') {
+        document.getElementById('about-me-text').style.display = 'block';
+    } else {
+        document.getElementById('about-me-text').style.display = 'none';
+    }
+}
